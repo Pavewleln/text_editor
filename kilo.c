6e1c1b8a3c07737bdf099c783713b2ba0e23d42c
pavel <kulikovps2004@gmail.com>
2023-09-03 20:57:14 +0300

    Entering raw mode

--- kilo.c
+++ kilo.c
@@ -1,4 +1,76 @@
-int main (int argc, char **argv) {
-    
+/*** includes ***/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <termios.h>
+
+/*** data ***/
+
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+// возвращаем все как было
+void disableRawMode()
+{
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+// отключаем канонический режим терминалы, выключаем отображение ввода в терминале? отключаем работу сочетаний cntrl + Z/C/S/Q/V
+void enableRawMode()
+{
+    // ECHO = отключить Эхо
+    // IXON = отключить cntrl + S/Q
+    // ICANON = отключить канонический режим терминала
+    // IEXTEN = отключить cntrl + V
+    // ICRNL = отключить cntrl + M
+    // ISIG = отключить cntrl + C/Z
+    // OPOST = отключить всю обработку вывода
+    // Когда BRKINTэтот параметр включен, условие останова приведет к SIGINTотправке сигнала в программу, например, при нажатии Ctrl-C.
+    // INPCK включает проверку четности, которая, похоже, не применима к современным эмуляторам терминала.
+    // ISTRIP приводит к удалению 8-го бита каждого входного байта, то есть ему присваивается значение 0. Вероятно, это уже отключено.
+    // CS8 это не флаг, это битовая маска из нескольких битов, которую мы устанавливаем с помощью оператора побитового ИЛИ ( |), в отличие от всех флагов, которые мы отключаем. Он устанавливает размер символа (CS) равным 8 бит на байт. В моей системе уже так настроено.
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_cflag &= ~(OPOST);
+    raw.c_cflag &= ~(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/*** init ***/
+
+int main(int argc, char **argv)
+{
+    enableRawMode();
+    while (1)
+    {
+        char c;
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if (iscntrl(c))
+        {
+            printf("%d\r\n", c);
+        }
+        else
+        {
+            printf("%d (%c)\r\n", c, c);
+        }
+        if (c == 'q')
+            break;
+    };
     return 0;
 }
